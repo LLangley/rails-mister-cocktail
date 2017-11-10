2b031ce2c2dbcd6b63301677321c99b57cf7e052
Luke Langley <langley0171@gmail.com>
2017-11-10 11:22:36 +0000

    cocktail app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+
+puts "creating cocktails..."
+ingredients_attributes = Ingredient.create([{ name: 'Lemon' }, { name: 'Ice' }, { name: 'Mint Leaves' }])
+
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+
+
+puts 'Finished!'
